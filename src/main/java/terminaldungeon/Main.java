92b8855fd0fe5c63866a755e2d6a89dcded0342a
hmw55 <hollandwesley55@gmail.com>
2026-01-31 12:02:17 -0700

    Update main to instantiate and start Game

--- src/main/java/terminaldungeon/Main.java
+++ src/main/java/terminaldungeon/Main.java
@@ -1,7 +1,16 @@
 package terminaldungeon;
 
+import terminaldungeon.game.Game;
+
+/*
+    Entry point for the application.
+    This class only exists to start the game.
+    It should stay very small
+*/
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Terminal Dungeon starting...");
+        Game game = new Game();
+        game.start();
     }
 }
